4068e5ff41feb10e9f42b18580b60a1e3f0709df
Milind <milindsharmagithub@gmail.com>
2023-06-26 19:54:33 +0530

    Added Weather API

---
+++ Basics8/index.js
@@ -0,0 +1,27 @@
+const loc = document.getElementById("location");
+const temp = document.getElementById("temp");
+const day = document.getElementById("day");
+const photo = document.getElementById("photo");
+const type = document.getElementById("type");
+const search = document.getElementById("searchBar");
+const btn = document.getElementById("searchBtn");
+
+const getWeather = async() => {
+    const url = `http://api.weatherapi.com/v1/current.json?key=ee64d5110b2f4b06829130230232606&q=${search.value}`;
+    const get = await fetch(url);
+    const data = await get.json();
+    console.log(data);
+    const time = data.location.localtime.split(" ")[0];
+    loc.innerText = data.location.name;
+    temp.innerText = data.current.temp_c + '°C';
+    day.innerText = getDay(new Date(time).getDay());
+    photo.src = data.current.condition.icon;
+    type.innerText = data.current.condition.text;
+}   
+
+const getDay = (number) => {
+    days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    return days[number];
+}
+
+btn.addEventListener("click",getWeather);
